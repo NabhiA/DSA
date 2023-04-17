876c50d41c98704caee81d5e8c5625fec9bb8a98
Nabhi Agrawal <nabhi.naresh2020@vitbhopal.ac.in>
2023-04-17 22:45:49 +0530

    Create pattern4.cpp

---
+++ pattern4.cpp
@@ -0,0 +1,175 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+
+/*Question:
+ABCD
+BCDE
+CDEF
+DEFG*/
+//     for (int i = 1; i <=n;  i++)
+
+//     {
+//         for (int j = 1; j <=n ; j++)
+//         {
+//             char ch= 'A'+i+j-2;
+//             cout<<ch;
+
+//         }
+//         cout<<endl;
+//     }
+//    
+
+// for (int i = 1; i <=n; i++)
+// {
+//     char ch='A'+i-1;
+
+//     for(int j =1; j<=n;j++){
+//         cout<<ch<<" ";
+//         ch++;
+
+
+//     }
+//     cout<<endl;
+// }
+
+
+
+
+
+
+
+
+
+
+    
+/*question:
+    ABCDE
+    FGHIJ
+    KLMNO
+    PQRST
+    UVWXY */
+
+//     char ch='A';
+//     int r=1;
+//     while (r<=n){
+//         int c=1;
+//         while (c<=n)
+//         {
+            
+//             cout<<ch;
+//             ch++;
+//             c++;
+//         }
+//         cout<<endl;
+//         r++;
+//     }
+// 
+
+
+
+
+
+
+
+
+/*QUestion:
+A
+BB
+CCC
+DDDD*/
+// 
+// for (int i = 1; i <= n; i++)
+// {
+//     for (int j = 1; j <=i; j++)
+
+//     {
+//         char ch='A'+i-1;
+//         cout<<ch;
+        
+        
+//     }
+    
+//     cout<<endl;
+
+// }
+
+
+
+
+
+
+
+/*question:
+
+A 
+B C 
+D E F 
+G H I J 
+
+*/
+// char ch='A';
+
+//     for (int i = 1; i <=n;  i++)
+
+//     {
+//         for (int j = 1; j <=i ; j++)
+//         {
+            
+//             cout<<ch<<" ";
+//             ch++;
+
+//         }
+//         cout<<endl;
+//     }
+//  
+
+
+
+
+
+
+
+/*Question:
+A 
+B C 
+C D E 
+D E F G 
+*/
+// for (int i = 1; i <= n; i++)
+// {
+    
+//     for (int j = 1; j <=i; j++)
+//     {
+//         char ch='A'+i+j-2;
+//         cout<<ch<<" ";
+
+//     }
+//     cout<<endl;
+// }
+
+
+// for (int i = 1; i <=n; i++)
+// {
+//     char ch='A'+i-1;
+//     for (int j = 0; j <i; j++)
+//     {
+//         cout<<ch<<" ";
+//         ch++;
+//     }
+//     cout<<endl;
+// }
+
+
+
+
+
+}
+
+
+
