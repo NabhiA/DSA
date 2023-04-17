8196a179f4ba8f33cb5b71ab737d156b4ee281c6
Nabhi Agrawal <nabhi.naresh2020@vitbhopal.ac.in>
2023-04-17 22:46:49 +0530

    Create patterns3.cpp

---
+++ patterns3.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+    
+ /* Question:
+
+1 
+2 3 
+3 4 5 
+4 5 6 7 
+*/  
+    
+    
+    
+    // int i=1;
+    
+    // for (int i = 1; i <=n; i++) 
+    // {
+    //     int value=i;   
+    //     for (int j = 1; j <=i ; j++)
+    //     {
+    //         cout<<value<<" ";
+    //         value++;
+            
+    //     }
+    //     cout<<endl;
+
+        
+    // }
+    
+
+    // int row=1;
+    // while (row<=n)
+    // {
+    //     int col=row;
+    //     while(col<row*2){
+    //         cout<<col<<" ";
+    //         col=col+1;
+
+    //     }
+    //     cout<<endl;
+    //     row=row+1;
+    // }
+
+
+
+
+
+
+
+
+/*Question:
+
+1 
+2 1 
+3 2 1 
+4 3 2 1 
+*/
+
+    // int i=1;
+    // while (i<=n)
+    // {
+    //     int j=i;
+    //     while (j>0)
+    //     {
+    //         cout<<j<<" ";
+    //         j--;
+
+    //     }
+    //     cout<<endl;
+    //     i++;  
+        
+    // }
+
+
+
+
+}
+
