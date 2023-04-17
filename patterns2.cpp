b4d3e979b8e45443bcecb42a451c5ae38e6e3bde
Nabhi Agrawal <nabhi.naresh2020@vitbhopal.ac.in>
2023-04-17 22:46:14 +0530

    Create patterns2.cpp

---
+++ patterns2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter n:";
+    cin>>n;
+
+
+
+/*Ques:
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+*/
+    int count=1;
+    
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <=i; j++)
+        {
+            cout<<count<<" ";
+            count++;
+
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
+
